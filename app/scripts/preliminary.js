130a6d487192c756475bb46faec7ce97e6c057ae
Jam Risser <jam@jamrizzi.com>
2016-04-02 15:15:22 -0500

    Added HashBang redirect

--- app/scripts/preliminary.js
+++ app/scripts/preliminary.js
@@ -1,15 +1,15 @@
-//forceHash();
+forceHashBang();
 importStyle();
 
-function forceHash() {
+function forceHashBang() {
   var url = window.location.href;
   var slashes = 0;
   for (var i = 0; i < url.length; i++) {
     if (url[i] === '/') {
       slashes++;
       if (slashes === 3) {
-        if (url[i + 1] && url[i + 1] !== '#') { // Hash not found
-          window.location.href = '/#' + url.substring(i, url.length);
+        if (url[i + 1] && url[i + 1] !== '#' && url[i + 2] !== '!') { // HashBang not found
+          window.location.href = '/#!' + url.substring(i, url.length);
         }
         break;
       }
