10bbddeaad9358786afbfc49ee5d4943b08a06eb
Jam Risser <jam@jamrizzi.com>
2017-02-28 04:49:15 -0600

    Fixed polyfill load and disable caching

--- app/core/scripts/app.js
+++ app/core/scripts/app.js
@@ -2,20 +2,24 @@
   'use strict';
   var app = document.getElementById('app');
 
-  _setBaseUrl();
-
-  _loadApp([
-    'settings',
-    'globals',
-    'authors',
-    'pages',
-    'taxonomies',
-    'style',
-    'theme'
-  ]).then((message) => {
-    window.setTimeout(function() {
-      _appLoaded();
-    }, 1000);
+  app.addEventListener('store-ready', () => {
+    window.addEventListener('WebComponentsReady', function() {
+      console.log('web components are ready');
+      _setBaseUrl();
+      _loadApp([
+        'settings',
+        'globals',
+        'authors',
+        'pages',
+        'taxonomies',
+        'style',
+        'theme'
+      ]).then((message) => {
+        window.setTimeout(function() {
+          _appLoaded();
+        }, 1000);
+      });
+    });
   });
 
   app.toastClicked = function() {
@@ -39,56 +43,54 @@
     console.log('Our app is ready to rock!');
   });
 
-  window.addEventListener('WebComponentsReady', function() {
-    console.log('web components are ready');
-  });
-})(document);
 
-function _setBaseUrl() {
-  var matches = window.location.href.match(/[\w\d\.:\/\\]+(?=\/#!)/g);
-  var baseUrl = window.location.href;
-  if (matches) {
-    baseUrl = matches[0];
-  } else {
-    if (baseUrl[baseUrl.length - 1] === '/') baseUrl = baseUrl.substring(0, baseUrl.length - 1);
+  function _setBaseUrl() {
+    var matches = window.location.href.match(/[\w\d\.:\/\\]+(?=\/#!)/g);
+    var baseUrl = window.location.href;
+    if (matches) {
+      baseUrl = matches[0];
+    } else {
+      if (baseUrl[baseUrl.length - 1] === '/') baseUrl = baseUrl.substring(0, baseUrl.length - 1);
+    }
+    store.dispatch({
+      type: SET_BASE_URL,
+      baseUrl: baseUrl
+    });
   }
-  store.dispatch({
-    type: SET_BASE_URL,
-    baseUrl: baseUrl
-  });
-}
 
-function _appLoaded() {
-  var state = store.getState();
-  var route = state.route;
-  var slugs = route.slugs ? route.slugs : {};
-  var parent = '/' + (slugs.parent ? slugs.parent : '');
-  var child = slugs.child ? '/' + slugs.child : '';
-  app.go.to(parent + child);
-  store.dispatch({ type: APP_LOADED });
-  document.getElementById('loading').innerHTML = '';
-  app.log.info(state.settings.title + ' loaded');
-}
+  function _appLoaded() {
+    var state = store.getState();
+    var route = state.route;
+    var slugs = route.slugs ? route.slugs : {};
+    var parent = '/' + (slugs.parent ? slugs.parent : '');
+    var child = slugs.child ? '/' + slugs.child : '';
+    app.log.info('ooops');
+    app.go.to(parent + child);
+    store.dispatch({ type: APP_LOADED });
+    document.getElementById('loading').innerHTML = '';
+    app.log.info(state.settings.title + ' loaded');
+  }
 
-function _loadApp(loaders) {
-  var promises = [];
-  _.each(loaders, (loader) => {
-    promises.push(_runLoader.bind(this, loader));
-  });
-  var promiseChain = Promise.resolve();
-  _.each(promises, (promise) => {
-    promiseChain = promiseChain.then(promise);
-  });
-  return promiseChain.then((res) => {
-    return res;
-  }).catch((err) => {
-    console.error(err);
-  });
-}
+  function _loadApp(loaders) {
+    var promises = [];
+    _.each(loaders, (loader) => {
+      promises.push(_runLoader.bind(this, loader));
+    });
+    var promiseChain = Promise.resolve();
+    _.each(promises, (promise) => {
+      promiseChain = promiseChain.then(promise);
+    });
+    return promiseChain.then((res) => {
+      return res;
+    }).catch((err) => {
+      console.error(err);
+    });
+  }
 
-function _runLoader(loader) {
-  var loaderElement = document.createElement('load-' + loader);
-  return loaderElement.load().then((res) => {
-    return res;
-  });
-}
+  function _runLoader(loader) {
+    var loaderElement = document.createElement('load-' + loader);
+    return loaderElement.load().then((res) => {
+      return res;
+    });
+  }
+})(document);
